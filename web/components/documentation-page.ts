f74b54df193c06b7e3957f29dd519a243d7fdfd2
Erik Moberg <extern.tretton37-moberg@universal-investment.com>
2022-09-13 12:11:59 +0200

    Added serve script; More doc

--- web/components/documentation-page.ts
+++ web/components/documentation-page.ts
@@ -27,7 +27,7 @@ export class DocumentationPage extends KnockoffJsBase<any> {
 }</code></pre>
 
         <h3>Bindings</h3>
-        <p>For property data binding, adding event listeners, etc <code>data-bind</code> is used. Example: <code>&lt;span data-bind="innerText: myText"&gt;&lt;/span&gt;</code> will display a span element and set its text to the value of <code>myText</code> in the state model. Generally, this can be used to set <i>any property</i> on an HTML element, such as <code>innerText</code> or <code>disabled</code>. Also nested properties can be set, such as <code>styles.display</code>. The target of the binding (in the example above <code>myText</code>), the value can be a property in the <code>state</code> object, or it can be a variable, getter, or function in the component that returns a value. Note that when using a function, only the name of the function should be used, without parenthesis. Example: <code>&lt;span data-bind="innerText: getText"&gt;&lt;/span&gt;</code> can be used to run the <code>getText</code> function in the component to get the text.</p>
+        <p>For property data binding, adding event listeners, etc <code>data-bind</code> is used. Example: <code>&lt;span data-bind="innerText: myText"&gt;&lt;/span&gt;</code> will display a span element and set its text to the value of <code>myText</code> in the state model. Generally, this can be used to set <i>any property</i> on an HTML element, such as <code>innerText</code> or <code>disabled</code>. Also nested properties can be set, such as <code>styles.display</code>. The target of the binding (in the example above <code>myText</code>) can be a property in the <code>state</code> object, or it can be a variable, getter, or function in the component that returns a value. Note that when using a function, only the name of the function should be used, without parenthesis. Example: <code>&lt;span data-bind="innerText: getText"&gt;&lt;/span&gt;</code> can be used to run the <code>getText</code> function in the component to get the text.</p>
         <p>To add an attribute, use the <code>attr</code> binding: <code>data-bind="attr: { class: getMyClass }"</code>. Note that if the method (<code>getMyClass</code> in this case) returns null, the attribute will be removed. (Pro tip: Generally, properties can be used instead of attributes.)</p> 
         <p>For events, the <code>event</code> binding is used, that should point to a function in the same class: <code>data-bind="event: { click: myHandler }"</code></p>
 
@@ -38,11 +38,13 @@ export class DocumentationPage extends KnockoffJsBase<any> {
         <pre><code>this.state.myArray = [...this.state.myArray, newElement];</code></pre>
 
         <h3>Routing</h3>
-        <p>Before doing anything else, set up the router to enable client side routing:</p>
+        <p>Before doing anything else, set up the router to enable client side routing. The example below would assume that there is a component named "home-page"
+        that is the start page of the application. It also assumes that there is a single <code>main</code> element on the page where it can be injected:</p>
         <pre><code>Router.init("home-page", "");</code></pre>
         <p>Any links starting with "/" should redirect to a registered web component. The component name should end with "-page" (for example "demo-page") and the link should point to the part before "-page" - so "/demo" will point to the component "/demo-page".</p>
+        
         <h3>CSS registry</h3>
-        <p>Since styles are isolated between components, they can be shared using the <code>CssRegistry</code>. To add an entry:</p>
+        <p>Styles are isolated between components by default, thanks to the shadow DOM, but styles can also can be shared using the <code>CssRegistry</code>. To add an entry with the key "common":</p>
         <pre><code>CssRegistry.register("common",
   \`code {
       color: black;
@@ -58,10 +60,12 @@ export class DocumentationPage extends KnockoffJsBase<any> {
   }</code></pre>
 
         <h3>Service locator</h3>
-        <p>To register a service:</p>
-        <pre><code>ServiceLocator.register(MovieService.name, () => new MovieService());</code></pre>
-        <p>To retrieve a service:</p>
+        <p>To register a service, provide a key and a factory method. In the example below, we use the name of the class as key:</p>
+        <pre><code>ServiceLocator.register(MovieService.name, () => new MovieService(ServiceLocator.resolve(MovieApi.name)));</code></pre>
+        <p>To retrieve a service in a component:</p>
         <pre><code>const movieService = ServiceLocator.resolve<MovieService>(MovieService.name)</code></pre>
+        <p>Note that while the component needs to access the service locator to resolve the service, the service itself will get all dependencies provided
+        automatically (by constructor injection) and does not need to access the service locator.</p>
         `;
   }
 
